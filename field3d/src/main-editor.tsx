9b0366ee04daaaba44507d8ea44c49736145e5b7
amorygalili <amorygalili@gmail.com>
2026-02-14 20:54:12 -0500

    Made the editor app distributable

---
+++ field3d/src/main-editor.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import RobotConfigEditor from "./editor/RobotConfigEditor";
+
+
+createRoot(document.getElementById("root")!).render(<RobotConfigEditor />);
